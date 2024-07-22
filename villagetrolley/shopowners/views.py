cc181a1cb01335226175c81be49a276a495008eb
cskraju <cskraju07@gmail.com>
2024-07-22 20:09:50 +0530

    ignore

--- villagetrolley/shopowners/views.py
+++ villagetrolley/shopowners/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from .forms import ShopOwnerRegistrationForm
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
-
-def register(request):
-    if request.method == 'POST':
-        form = ShopOwnerRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Registration successful!')
-            return redirect('login')  
-    else:
-        form = ShopOwnerRegistrationForm()
-    
+def customer_registration_form(request):
+    form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+
+
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = ShopOwnerRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Registration successful!')
+#             return redirect('login')  
+#     else:
+#         form = ShopOwnerRegistrationForm()
+    
+#     return render(request, 'register.html', {'form': form})
 
